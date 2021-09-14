28442d5cf0ae034d729e15c95ebd02c14514ef3b
zihan <zihanhannah@gmail.com>
2021-09-14 11:04:02 -0500

    using setLayout() to make the dafult CardLayout to FlowLayout, GridLayout, Null... to make all the label showed in the frame

--- src/main/java/org/example/FirstGuiJFrame.java
+++ src/main/java/org/example/FirstGuiJFrame.java
@@ -1,8 +1,9 @@
 package org.example;
 
 import javax.swing.*;
+import java.awt.*;
 
-
+// JFrame follows the CardLayout: only show the top card as default
 public class FirstGuiJFrame extends JFrame {
     public FirstGuiJFrame() {
 //        make the frame visible
@@ -10,10 +11,13 @@ public class FirstGuiJFrame extends JFrame {
 //        set the frame size
         setSize(400,400);
 
-
+//        changing the default CardLayout to FlowLayout, GridLayout, Null.....
+        setLayout(new GridLayout());
         JLabel label1 = new JLabel("Hello World");
+        JLabel label2 = new JLabel("Welcome Navin");
 //        adding letters in the frame
         add(label1);
+        add(label2);
     }
 
     public static void main(String[] args) {
