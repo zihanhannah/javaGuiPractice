0f6371ef6ca1da8bd02a0a3c8344e4ffbc92f82e
zihan <zihanhannah@gmail.com>
2021-09-14 12:44:17 -0500

    how to do the EventListener

---
+++ src/main/java/org/example/AddGui.java
@@ -0,0 +1,49 @@
+package org.example;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class AddGui extends JFrame implements ActionListener {
+    JTextField t1,t2;
+    JButton b;
+    JLabel label;
+
+    public AddGui()  {
+//        create a TextField with 20 space
+       t1 = new JTextField(20);
+       t2 = new JTextField(20);
+
+       b = new JButton("OK");
+       label = new JLabel("Result:");
+
+        add(t1);
+        add(t2);
+        add(b);
+        add(label);
+
+//        add EventListener, ActionListener is an Interface
+//        this should be the object of AddGui and Object of ActionListener
+        b.addActionListener(this);
+
+        setVisible(true);
+        setSize(400,400);
+        setLayout(new FlowLayout());
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+//    everytime when we click the button, it will call the actionPerformed method
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        int num1 =Integer.parseInt(t1.getText());
+        int num2=Integer.parseInt(t2.getText());
+
+        int value = num1 + num2;
+        label.setText(" "+ value);
+    }
+
+    public static void main(String[] args) {
+        AddGui addition1 = new AddGui();
+    }
+}
--- src/main/java/org/example/FirstGuiJFrame.java
+++ src/main/java/org/example/FirstGuiJFrame.java
@@ -6,18 +6,24 @@ import java.awt.*;
 // JFrame follows the CardLayout: only show the top card as default
 public class FirstGuiJFrame extends JFrame {
     public FirstGuiJFrame() {
-//        make the frame visible
-        setVisible(true);
-//        set the frame size
-        setSize(400,400);
-
-//        changing the default CardLayout to FlowLayout, GridLayout, Null.....
-        setLayout(new GridLayout());
         JLabel label1 = new JLabel("Hello World");
         JLabel label2 = new JLabel("Welcome Navin");
 //        adding letters in the frame
         add(label1);
         add(label2);
+
+
+//        todo : the must have
+//        make the frame visible
+        setVisible(true);
+//        set the frame size
+        setSize(400,400);
+//        changing the default CardLayout to FlowLayout, GridLayout, Null.....
+        setLayout(new FlowLayout());
+//        make the exit button on the Frame work( terminate the program): two ways
+//        setDefaultCloseOperation(3);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
     }
 
     public static void main(String[] args) {
